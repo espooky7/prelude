2a686fdbab7a8d75be5f709d1268789a036f3e6f
Emily Dahlberg <emily.dahlberg@edahlseaosxl1.bigfish.lan>
2014-04-02 15:25:52 -0700

    Adding custom.el file

---
+++ personal/custom.el
@@ -0,0 +1,9 @@
+;; Pull in custom packages
+(prelude-require-packages '(twilight-theme sublime-themes))
+
+;; Load my Theme of Choice
+(load-theme 'junio t)
+
+(provide 'custom)
+
+;;; custom.el ends here
